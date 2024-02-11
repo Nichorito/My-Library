cda5789dd121a5ed52198a4a94612d7667c7cb89
Nicholas <nsheppard999@gmail.com>
2024-02-10 23:37:13 -0400

    Close form on submit + style changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let addBookButton = document.querySelector('#add-book');
+let submitButton = document.querySelector('#submit');
 
 addBookButton.addEventListener('click', () => {
-    alert('Add a book?  Sorry we dont do that yet');
     const form = document.querySelector('#form-container');
 
     if (form.style.display === 'none') {
@@ -13,3 +13,7 @@ addBookButton.addEventListener('click', () => {
       }
 });
 
+//Remove form overlay when submit is clicked
+submitButton.addEventListener('click', () => {
+    form.style.display = 'none';
+})
