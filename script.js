acd02fbb3920d23ada5d41cb339fe0135869874c
Nicholas <nsheppard999@gmail.com>
2024-02-11 15:24:54 -0400

    Dynamically add child divs to main grid
    New books can now be added using the +Add book
    button and they will show up in the grid properly

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+/////////////////////////////////////
+//OPEN AND CLOSE BOOK ADDING FORM
+/////////////////////////////////////
 let addBookButton = document.querySelector('#add-book');
-let submitButton = document.querySelector('#submit');
+const form = document.querySelector('#form-container');
 
 addBookButton.addEventListener('click', () => {
-    const form = document.querySelector('#form-container');
+    
 
     if (form.style.display === 'none') {
         // 👇️ this SHOWS the form
@@ -13,7 +16,73 @@ addBookButton.addEventListener('click', () => {
       }
 });
 
-//Remove form overlay when submit is clicked
-submitButton.addEventListener('click', () => {
+//////////////////////////////////////
+/////// CREATE CHILD UPON SUBMIT //////
+//////////////////////////////////////
+
+const submit = document.querySelector('#submit');
+const grid = document.querySelector('#main');
+
+submit.addEventListener('click', () => {
+    event.preventDefault();
+
+    //Get book information
+    const name = document.querySelector('#name').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    //Create book object
+    const book = new Book(name, author, pages); 
+
+    //Debug log
+    console.log(book.info())
+
+    //Create child div
+    const newChild = document.createElement('div')
+    newChild.classList.add('child-item');
+
+    //Create uniquie ID for new div
+    newChild.id = name.replace(/\s+/g, '-').toLowerCase();
+
+    //Create innerHTML for new child
+    newChild.innerHTML = `
+    <div class="info-container">
+                <h2 class="book-title">${name}</h2>
+            </div>
+
+            <div class="author-pages-container">
+                <h3 class="author-main">By:  ${author}</h3>
+                <h4 class="pages">${pages} Pgs.</h4>
+            </div>
+            <div class="button-container">                
+                <div class="toggle-container">
+                    <h3>Read?</h3>
+                    <button class="toggle-main">Toggle</button>
+                    
+                </div>
+                <button class="delete">Delete</button>
+            </div>`
+
+
+    //Append child to grid
+    grid.appendChild(newChild)
+
+    //Hide form
     form.style.display = 'none';
 })
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return (`${title} by ${author}, ${pages} pages, ${read}`)
+  }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "already read");
+
+
+console.log(theHobbit.info());
